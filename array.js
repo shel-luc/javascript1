36acc9828481bf54df8ca5808eb36ffea3609ce8
shel-luc pubien <shelucpubien@gmail.com>
2019-07-28 00:36:55 -0400

    new array

---
+++ array.js
@@ -0,0 +1,14 @@
+let arr= [1, 2, 3, 4]
+console.log(arr[3])
+console.log(arr.reverse())
+// map and filter
+let stud =[{name:"louk", age:28},
+            {name:"jules", age:30},
+            {name:"Max", age:11}]
+
+for (i = 0; stud.length > i ; i++){
+
+      console.log (stud[i].age)
+     }
+ 
+      
